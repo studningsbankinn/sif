fc8a30f112153d0fe300bf872747819d87362af7
bergur <bergur@bergur.is>
2021-04-14 22:09:51 +0000

    use path to select school

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@
       <thead>
         <tr>
           <td>Skóli</td>
+          <td>Flokkur</td>
           <td>Spurning</td>
-          <td>Svar</td>
+          <td>Svar</td>          
+          <td>Athugasemd</td>
         </tr>
       </thead>
       <tbody>
@@ -14,8 +16,10 @@
           :key="row.id"
         >
           <td>{{ row.placeName }}</td>
+          <td>{{ row.questionCategoryName }}</td>
           <td>{{ row.question }}</td>
-          <td>{{ row.answer }}</td>        
+          <td>{{ row.answer }}</td>          
+          <td>{{ row.comment }}</td>
         </tr>
       </tbody>
     </table>
@@ -31,14 +35,34 @@ export default {
       answers: []
     }
   },
-  created() {
+  methods: {
+    mapUrlToSchool () {
+      const url = window.location.toString()      
+      const parts = url.split('/')
+      
+      const lastItem = parts[parts.length -1]
+      const secondLastItem = parts[parts.length - 2]
+      const school = (lastItem === '') ? secondLastItem : lastItem
+      
+      const map = {
+        'verzlunarskoli-islands': 1
+      }
+
+      return map[school]      
+      
+    }
+  },
+  created() {    
+    console.log('created')
+    const schoolId = this.mapUrlToSchool()
     agent
-      .get(process.env.STUDNINGSBANKINN_API_URL + '/answers')
+      .get(process.env.STUDNINGSBANKINN_API_URL + '/answers?placeId=' + schoolId)
       .set('Authorization', 'Bearer ' + process.env.STUDNINGSBANKINN_API_KEY)
       .then(data => {
-        console.log('fékk svar')
-        console.log(data.body)
-        this.answers = data.body        
+        this.answers = data.body
+      })
+      .catch(e => {
+        console.log(e)
       })
   }
 }
