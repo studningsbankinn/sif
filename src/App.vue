649ba5f3e4515ce596da553c0530a49d4f8284bd
Brynja Dagmar <brynjadagmar@gmail.com>
2021-04-19 10:00:58 +0000

    Versló fikt

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,57 @@
 <template>
   <div>
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+      <!-- <div class="card-image"> -->
+            <figure class="image is-64x64">
+              <img src="/images/verzlo_logo.jpg" alt="Placeholder image">
+            </figure>
+          </div>
+
+    <!-- <div class="media-content"> -->
+          <div class="media-left">
+            <p class="title is-4">Verzlunarskóli Íslands</p>
+            <div class="media-left">
+              Lorem ipsum dolor sit amet,
+            </div>
+            <p class="subtitle is-6">@johnsmith</p>
+            <i class="fas fa-globe"></i>
+            <a href="https://verslo.is">verslo.is</a>
+          </div>
+          <div class="media-left">
+            <p class="title is-5">Tengiliður</p>
+            <p class="media-left">Berglind Helga Sigurþórsdóttir</p>
+            <p class="media-left">Sími 5900615</p>
+          </div>
+
+
+        </div>
+      </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">John Smith</p>
+        <p class="subtitle is-6">@johnsmith</p>
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br>
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+    </div>
+  </div>
+</div>
+
     <table class="table table-striped">
       <thead>
         <tr>
